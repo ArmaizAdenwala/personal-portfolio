504468881fa15ebb457c97dd725e8a2fbbb17856
Armaiz Adenwala <armaiz.adenwala@gmail.com>
2020-01-26 09:03:25 -0800

    fix fonts

--- src/components/home/about/About.js
+++ src/components/home/about/About.js
@@ -15,8 +15,10 @@ export default class About extends Component {
             <div className="g__col-7">
               <h3 className="tg__title-4 about__title">Who am I?</h3>
               <p className="tg__title-6 about__bio">
-                I'm a passionate Software Engineer from Sacramento, CA who's
-                experienced in taking fullstack apps to production from scratch.
+                I'm a passionate Software Engineer from{' '}
+                <span className="about__bio--emphasis">Sacramento, CA</span>{' '}
+                who's experienced in taking fullstack apps to production from
+                scratch.
                 <br />
                 <br />
                 I've had the oppurtunity to develop for over 5 awesome companies
