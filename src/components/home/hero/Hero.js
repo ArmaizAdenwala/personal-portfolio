e29f02d3b17b389996c12c2c9c8829a5161eae2c
Armaiz Adenwala <armaiz.adenwala@gmail.com>
2020-01-27 11:52:33 -0800

    fix hero responsiveness

--- src/components/home/hero/Hero.js
+++ src/components/home/hero/Hero.js
@@ -10,14 +10,23 @@ export default class Hero extends Component {
           <div className="hero__content">
             <h1
               className="
-              tg__title-2
+              tg__title-4--s
+              tg__title-3--m
+              tg__title-2--l
               hero__content__title
               "
             >
               {"Hi, I'm "}
               <span className="tg__c--primary tg__w--400">Armaiz Adenwala</span>
             </h1>
-            <h2 className="tg__title-4 hero__content__subtitle">
+            <h2
+              className="
+              tg__title-4--l
+              tg__title-5--m
+              tg__title-6--s
+              hero__content__subtitle
+            "
+            >
               I'm a Software Engineer that welcomes challenges and provides
               solutions.
             </h2>
