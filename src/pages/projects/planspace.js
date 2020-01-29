d8d92b23f815018e78cf2e0c795d425f9d7eba22
Armaiz Adenwala <armaiz.adenwala@gmail.com>
2020-01-29 15:51:38 -0800

    update images

--- src/pages/projects/planspace.js
+++ src/pages/projects/planspace.js
@@ -6,6 +6,8 @@ import '../../styles/pages/Home.scss';
 import Layout from '../../components/layout';
 import SEO from '../../components/seo';
 import CreateTaskImage from './planspace/create_task_planspace_react_native_app.png';
+import ViewTaskImage from './planspace/view_task_planspace_node_js_socket_io_app.png';
+import EditProfileImage from './planspace/edit_profile_invite_planspace_ruby_on_rails_app.png';
 
 const IndexPage = () => (
   <Layout>
@@ -22,6 +24,21 @@ const IndexPage = () => (
           'App designed for roommates to have automated chore schedules on a week by week basis. App designed for roommates to have automated chore schedules on a week by week basis.'
         }
         src={CreateTaskImage}
+      />
+      <ProjectSection
+        flip
+        title={'Manage '}
+        description={
+          'App designed for roommates to have automated chore schedules on a week by week basis. App designed for roommates to have automated chore schedules on a week by week basis.'
+        }
+        src={ViewTaskImage}
+      />
+      <ProjectSection
+        title={'Manage Roommates'}
+        description={
+          'App designed for roommates to have automated chore schedules on a week by week basis. App designed for roommates to have automated chore schedules on a week by week basis.'
+        }
+        src={EditProfileImage}
       />
     </div>
   </Layout>
