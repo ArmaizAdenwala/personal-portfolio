4a746784b5300897c4d79d094eae2cd09959be96
Armaiz Adenwala <armaiz.adenwala@gmail.com>
2020-01-29 15:43:03 -0800

    create project section

--- src/components/projects/projectSection/ProjectSection.js
+++ src/components/projects/projectSection/ProjectSection.js
@@ -3,12 +3,17 @@ import { Container } from '../../../components/general';
 import './ProjectSection.scss';
 
 export default class ProjectSection extends Component {
+  _getDirection = () =>
+    this.props.flip ? 'flex--row-reverse--m flex--row-reverse--l' : '';
   render() {
     return (
       <div className="project-section">
         <Container>
-          <div className="g__row">
-            <div className="g__col-7">
+          <div className={`g__row ${this._getDirection()}`}>
+            <div className="g__col-5">
+              <img className="project-section__img" src={this.props.src} />
+            </div>
+            <div className="g__col-7 flex--v-center">
               <h3
                 className="
                   tg__title-5--s
@@ -17,21 +22,19 @@ export default class ProjectSection extends Component {
                   tg__t--center--s
                   tg__c--primary
                   tg__w--700
+                  m-v--8
                 "
               >
                 {this.props.title}
               </h3>
               <p
                 className="
-              tg__title-5
-              tg__lh--1-75
-            "
+                  tg__title-5
+                  tg__lh--1-75
+                "
               >
                 {this.props.description}
               </p>
-            </div>
-            <div className="g__col-5">
-              <img className="project-section__img" src={this.props.src} />
             </div>
           </div>
         </Container>
