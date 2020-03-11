bd7bd11ec6837544a96463605b86bf20292bdc04
Armaiz Adenwala <armaiz.adenwala@gmail.com>
2020-03-11 11:31:06 -0700

    reduce bundle size

--- src/components/content/codeBlock/CodeBlock.js
+++ src/components/content/codeBlock/CodeBlock.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import {LightAsync as SyntaxHighlighter, PrismAsyncLight as Prism} from 'react-syntax-highlighter';
+import {PrismAsyncLight as Prism} from 'react-syntax-highlighter';
 
 const CodeBlock = ({ children, language, useHighlight }) => {
-  if (useHighlight) {
-    return (
-      <SyntaxHighlighter language={language} useInlineStyles={false}>
-        {children}
-      </SyntaxHighlighter>
-    );
-  }
   return (
     <Prism language={language} useInlineStyles={false}>
       {children}
