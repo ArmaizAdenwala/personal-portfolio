6c1d89565142486e9f61044f2cab1563ad9bbdbd
Armaiz Adenwala <armaiz.adenwala@gmail.com>
2020-03-11 03:07:43 -0700

    remove unused bundles

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-preact',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
